2aeb01c2dd2b805942de36e455cf7d3acdd45123
Noyon Rahman <noyonrahman2003@gmail.com>
2025-11-27 21:19:50 +0600

    Refactor application structure and implement contact management features
    - Remove the App component and replace it with a Root component that handles routing.
    - Implement contact management actions: create, edit, and delete contacts.
    - Add loaders for fetching contacts and individual contact details.
    - Create pages for displaying and editing contacts, including error handling.
    - Update CSS styles to support the new layout and components.
    - Integrate localForage for persistent contact storage.
    - Add search functionality for contacts.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {
+  createContactAction,
+  destroyContactAction,
+  editContactAction,
+} from "@/actions/contact";
+import { getContactLoader, getContactsLoader } from "@/loaders/contact";
+import { Contact, EditContact, ErrorPage } from "@/pages";
+import Root from "@/Root";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: getContactsLoader,
+    action: createContactAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: getContactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: getContactLoader,
+        action: editContactAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyContactAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
---
+++ src/pages/index.jsx
@@ -0,0 +1,3 @@
+export { default as Contact } from "./Contact";
+export { default as EditContact } from "./EditContact";
+export { default as ErrorPage } from "./Error";
