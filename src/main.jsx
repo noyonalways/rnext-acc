cf11f77c5290dcd2b6d643a98b7a88249bcb41cf
Noyon Rahman <noyonrahman2003@gmail.com>
2025-11-29 20:18:02 +0600

    feat: Implement CRUD functionality for posts with Axios integration
    - Created main App component to manage posts state and handle API requests.
    - Added AddPost component for creating new posts.
    - Implemented EditPost component for updating existing posts.
    - Developed Posts component to display the list of posts with delete and edit options.
    - Set up Axios API instance with request and response interceptors for error handling.
    - Included mock data for initial posts in db.js and db.json.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import router from "@/routes";
+import App from "@/App";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 );
